d8ae205a1d96a265bc2dd4b0da34aacae29decdd
Kawahara Junichi <kawahara-j@IMAC5-6.local>
2020-03-03 17:28:48 +0900

    Dispatcher の作成

--- src/js/stores/TodoStore.js
+++ src/js/stores/TodoStore.js
@@ -1,4 +1,5 @@
 import { EventEmitter } from "events";
+import dispatcher from "../dispatcher"
 
 class TodoStore extends EventEmitter {
     constructor() {
@@ -32,8 +33,18 @@ class TodoStore extends EventEmitter {
     getAll() {
         return this.todos;
     }
+
+    handleActions(action) {
+        switch (action.type) {
+            case "CREATE_TODO": {
+                this.createTodo(action.text);
+            }
+        }
+    }
 }
 
 const todoStore = new TodoStore;
+dispatcher.register(todoStore.handleActions.bind(todoStore));
 
+window.dispatcher = dispatcher;
 export default todoStore;
