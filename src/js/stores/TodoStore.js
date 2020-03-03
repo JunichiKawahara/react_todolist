0f04e89b53aae8e9d6d34f34cfa6e1115f989c58
Kawahara Junichi <kawahara-j@IMAC5-6.local>
2020-03-03 17:22:31 +0900

    動的にTodo リストが更新されるようにする

--- src/js/stores/TodoStore.js
+++ src/js/stores/TodoStore.js
@@ -1,1 +1,39 @@
-/* This file will be filled with on the lesson */
+import { EventEmitter } from "events";
+
+class TodoStore extends EventEmitter {
+    constructor() {
+        super();
+        this.todos = [
+            {
+              id: 113464613,
+              text: "Go Shopping",
+              complete: false
+            },
+            {
+              id: 235684679,
+              text: "Pay Water Bills",
+              complete: false
+            }
+        ];
+    }
+
+    createTodo(text) {
+        const id = Date.now();
+
+        this.todos.push({
+            id,
+            text,
+            complete: false
+        });
+
+        this.emit("change");
+    }
+
+    getAll() {
+        return this.todos;
+    }
+}
+
+const todoStore = new TodoStore;
+
+export default todoStore;
